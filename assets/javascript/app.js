3085b24aa74b3efcfc6c04ad8b1d313fd5e3461c
Sweet Ace <lakshmialeman@gmail.com>
2018-09-20 00:18:49 -0700

    updated layout of gifs being displayed and added some bonus items in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,93 +6,216 @@
 var topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbill", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 
 // giphy api: http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=";
+var queryUrl = "http://api.giphy.com/v1/gifs/search?q=";
 
+//gif container
+var gifDiv;
 
-//make a list of links based on the list of topics
-for (var i = 0; i < topics.length; i++) {
-    //changes spaces to + it helps to build the url
-    //build topic url
-    //let is necessary given scope issues
-    let topicUrl = queryUrl + topics[i].replace(/\s+/g, '+') + "&api_key=GNoX2M7isIvDEl7iH6lwnneiUlpYTSVa&limit=10";
+//gif image container
+var imageStill;
 
-    //make a dropdown item to be added to animal dropdown
-    var listItem = $("<a class=\"dropdown-item\" href=\"#\">" + topics[i] + "</a>");
+//container that holds gif rating and title
+var gifDataDiv;
 
-    //list item onclick function
-    $(listItem).click(function () {
-        //put ajax call here
-        console.log(topicUrl);
-        //make call to the api
-        $.ajax({
-            url: topicUrl,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-            // Storing an array of results in the results variable
-            var results = response.data;
-            //empty rows
-            $("#firstRow").empty();
-            $("#secondRow").empty();
-            $("#thirdRow").empty();
+//creates p to hold gif data
+var p;
 
-            //go through the data of the giphy object
-            for (var i = 0; i < results.length; i++) {
-                //create template for the gif to be displayed
-                // Creating a div for the gif
-                var gifDiv = $("<div class=\"col-sm-3\">");
+//creates strong tag that holds the labels to the gif data
+var bold;
 
-                //display rating
-                var p = $("<p>");
-                //console.log(results[i].rating);
+//access gif images from the api
+var resultsImg;
 
-                //display gifs
-                var imageStill = $("<img class=\"img-fluid gif\"/>");
+//will hold gif rating data
+var rating;
 
-                //access gif images
-                var resultsImg = results[i].images;
+//will hold gif title data
+var title;
 
-                //accesss gif ratings
-                var rating = results[i].rating;
+//download gif
+var downloadGif;
 
-                //the first 4 (index: 0,1,2,3) will be saved on the firstRow
-                if (i <= 3) {
-                    //build gif and show in the html
-                    initGif("#firstRow", gifDiv, p, imageStill, resultsImg,rating);
-                } else if (4 <= i && i <= 7) { //the second 4 (index: 4,5,6,7will be saved on the secondRow
-                    //build gif and show in the html
-                    initGif("#secondRow", gifDiv, p, imageStill, resultsImg,rating);
-                } else if (i <= 9) { //the third 4 (index: 8,9) will be saved on the thirdRow
-                    //build gif and show in the html
-                    initGif("#thirdRow", gifDiv, p, imageStill, resultsImg,rating);
-                }
-            }
-            //have the gif animate after the user has clicked it
-            $(".gif").on("click", function () {
-                //initiate the data state of the current gif
-                var state = $(this).attr("data-state");
-                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                if (state === "still") {
-                    //set the src of the image to be its animated url
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    //set data state to animate
-                    $(this).attr("data-state", "animate");
-                } else {
-                    //set the scr of the image to be its still url
-                    $(this).attr("src", $(this).attr("data-still"));
-                    //set data state to still
-                    $(this).attr("data-state", "still");
-                }
+//current category
+var currCategory;
+
+//current offset
+var offsetCount = 0;
+
+//adds on to the list of animals
+function addAnimal() {
+    //make a list of links based on the list of topics
+    for (var i = 0; i < topics.length; i++) {
+        //initialize category to the topics inside the array
+        //changes spaces to + it helps to build the url
+        let tempCategory = topics[i].replace(/\s+/g, '+');
+
+        //make a dropdown item to be added to animal dropdown
+        var listItem = $("<a class=\"dropdown-item\" href=\"#\">" + topics[i] + "</a>");
+
+        //remove click handler
+        $(listItem).off("click");
+
+        //list item onclick function
+        $(listItem).click(function () {
+            //initialize currCategory
+            currCategory = tempCategory;
+
+            //set offset to 0 when new category is clicked
+            offsetCount = 0;
+
+            //build topic url
+            //let is necessary given scope issues
+            let topicUrl = makeUrl();
+
+            //empty columns
+            $("#gifColumns").empty();
+
+            //initializes the topics to be used in the dropdown
+            callApi(topicUrl);
+        });
+
+        //add list of topics onto the html
+        $(".dropdown-menu").append(listItem);
+    }
+}
+//loads jQuery after the document is already loaded
+$(document).ready(function () {
+    //initialize dropdown list
+    addAnimal();
+
+    //initialize currCategory
+    currCategory = "animals";
+
+    //set offset to 0 
+    offsetCount = 0;
+
+    //loads a list of 10 animals
+    callApi(makeUrl());
+
+    //remove click handler
+    $("#viewMore").off("click");
+
+    //add more 10 more gifs
+    $("#viewMore").click(function (event) {
+        //update offset count
+        offsetCount += 10;
+
+        //loads a list of 10 animals
+        callApi(makeUrl());
+    });
+});
+function makeUrl() {
+    //set up  url
+    return queryUrl + currCategory + "&api_key=GNoX2M7isIvDEl7iH6lwnneiUlpYTSVa&limit=10&offset=" + offsetCount;
+}
+function callApi(url) {
+    //put ajax call here
+    console.log(url);
+
+    //make call to the api
+    $.ajax({
+        url: url,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        // Storing an array of results in the results variable
+        var results = response.data;
+
+        //go through the data of the giphy object
+        for (var i = 0; i < results.length; i++) {
+            //create template for the gif to be displayed
+            // Creating a div for the gif
+            gifDiv = $("<div class=\"card text-center border border-dark shadow-lg\">");
+
+            //display gif image
+            imageStill = $("<img class=\"card-img-top img-fluid border-bottom border-dark gif\"/>");
+
+            //create a div to hold the ratings and title of the gif
+            gifDataDiv = $("<div class=\"card-body\">");
+
+            //display rating
+            p = $("<p>");
+
+            //add bold tag
+            bold = $("<strong>");
+
+            //access gif images
+            resultsImg = results[i].images;
+
+            //download url
+            let downloadUrl = resultsImg.downsized.url;
+
+            //slug name
+            let slug = results[i].slug;
+
+            //add download button
+            downloadGif = $("<a href=\"#\" class=\"btn btn-primary\">");
+
+            //accesss gif ratings
+            rating = results[i].rating;
+
+            //access title of the gif
+            title = results[i].title;
+
+            //build gif columns and show in the html
+            initGif("#gifColumns", gifDiv, gifDataDiv, p, bold, imageStill, resultsImg, rating, title, downloadGif);
+
+            //download click so the user can have access to the gif from the computer
+            downloadGif.click(function () {
+
+                var x = new XMLHttpRequest();
+                x.open("GET", downloadUrl, true);
+                x.responseType = 'blob';
+                x.onload = function (e) { download(x.response, slug + ".gif", "image/gif"); }
+                x.send();
             });
+        }
+        //remove click handler
+        $(".gif").off("click");
+
+        //have the gif animate after the user has clicked it
+        $(".gif").on("click", function () {
+            //initiate the data state of the current gif
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            if (state === "still") {
+                //set the src of the image to be its animated url
+                $(this).attr("src", $(this).attr("data-animate"));
+                //set data state to animate
+                $(this).attr("data-state", "animate");
+            } else {
+                //set the scr of the image to be its still url
+                $(this).attr("src", $(this).attr("data-still"));
+                //set data state to still
+                $(this).attr("data-state", "still");
+            }
         });
     });
-
-    //add list of topics onto the html
-    $(".dropdown-menu").append(listItem);
 }
 
+//remove click handler
+$("#runAddAnimal").off("click");
+
+//add animal to the topics array with the submit click function
+$("#runAddAnimal").click(function (event) {
+    // This line allows us to take advantage of the HTML "submit" property
+    // This way we can hit enter on the keyboard and it registers the search
+    // (in addition to clicks). Prevents the page from reloading on form submit.
+    event.preventDefault();
+
+    //target input from add animal input box and add it to the topics array
+    topics.push($("#addAnimal").val().trim());
+    // console.log($("#addAnimal").val().trim());
+
+    //empty dropdown menu
+    $(".dropdown-menu").empty();
+
+    //add onto the list of animals
+    addAnimal();
+});
+
 //initializes gif 
-function initGif(row, gifDiv, p, imageStill, resultsImg, rating){
+function initGif(row, gifDiv, gifDataDiv, p, bold, imageStill, resultsImg, rating, title) {
     //adds in the col-sm-3
     $(row).append(gifDiv);
 
@@ -108,12 +231,27 @@ function initGif(row, gifDiv, p, imageStill, resultsImg, rating){
     //sets animate data state
     imageStill.attr("data-state", "still");
 
-    //adds gif
+    //adds gif image
     gifDiv.append(imageStill);
 
-    //adds p tag
-    gifDiv.append(p);
+    //adds gif text below image
+    gifDiv.append(gifDataDiv);
 
-    //adds content to p tag
-    p.text("Rating: " + rating);
+    //adds rating text
+    bold.text("Rating: ");
+
+    //adds rating value
+    p.text(rating);
+
+    //adds <strong> tag after <p> tag
+    p.prepend(bold);
+
+    //adds rating p tag
+    gifDataDiv.append(p);
+
+    //adds title p tag
+    gifDataDiv.append(p.clone().text(title).prepend(bold.clone().text("Title: ")));
+
+    //adds download button
+    gifDataDiv.append(downloadGif.text("Download"));
 }
